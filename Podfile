2cd3ea06dc043788dbeba02584e12da0d6a51d28
roeyHonig <roeyhonig94@gmail.com>
2018-06-16 10:25:33 +0300

    1st commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SDWebImageFramework' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SDWebImageFramework
+
+pod 'SDWebImage/Core'
+pod 'SDWebImage/MapKit'
+pod 'SDWebImage/WebP'
+pod 'SDWebImage/GIF'
+
+end
--- SDWebImageFramework/ViewController.swift
+++ SDWebImageFramework/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://1.bp.blogspot.com/-gtf1BZfH2xw/Ujkeb5kVVxI/AAAAAAAAA3I/iIElhyyLXM4/s1600/leopard+iphone+5+free+background.jpg")
+        imageView.sd_setImage(with: url) { (image, err, sdImageCatchType, url) in
+            // nothing
+            print("error: \(err?.localizedDescription)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +30,4 @@ class ViewController: UIViewController {
 
 }
 
+// https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6mNrVztfbAhWrJcAKHVsvCWwQjRx6BAgBEAU&url=http%3A%2F%2Fbelinspired.blogspot.com%2F2013%2F09%2Fmore-iphone-5-backgrounds.html&psig=AOvVaw2udUoV_jDzHTgS3F96HrJU&ust=1529218944285624
